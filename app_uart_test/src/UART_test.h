f7426c48d6551648e4e7b0ec045d8b055cc95ba8
dan <dan@xmos.com>
2011-03-02 12:04:21 +0000

    added copyright headers and LICENSE.txt

--- app_uart_test/src/UART_test.h
+++ app_uart_test/src/UART_test.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2011, XMOS Ltd, All rights reserved
+// This software is freely distributable under a derivative of the
+// University of Illinois/NCSA Open Source License posted in
+// LICENSE.txt and at <http://github.xcore.com/>
+
 #define BIT_RATE 96000
 #define BITS_PER_BYTE 8
 #define SET_PARITY 2
